2ba43e938c10eb90c6e366703febbf787ce18a72
Влад Фомин <mr.vladyslavfomin@gmail.com>
2020-02-01 18:30:32 +0200

    Removing unused directives

--- FlashSpyCore/FlashSpyDbContext.cs
+++ FlashSpyCore/FlashSpyDbContext.cs
@@ -1,8 +1,6 @@
 ﻿using FlashSpyCore.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace FlashSpyCore
 {
--- FlashSpyCore/Models/Level.cs
+++ FlashSpyCore/Models/Level.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FlashSpyCore.Models
+﻿namespace FlashSpyCore.Models
 {
     class Level
     {
